6fd1378a6f119e7fe00b2bbb33c288ea2c1c5c57
outDream <2428091608@qq.com>
2016-06-23 17:11:37 +0800

    Update some code

--- VideoProcessing/Segment.cpp
+++ VideoProcessing/Segment.cpp
@@ -5,20 +5,23 @@
 #include<math.h>
 
 #include "opencv2/highgui/highgui.hpp"
-#include<io.h>
+#include <io.h>
 #include <cv.h>
 #include <ml.h>
 using namespace std;
 using namespace cv;
 
-void dataToLibSVM(vector<vector<double>>& vfeatures, vector<int>& vlabels, string outputFile);
 void svm_train(vector<vector<double>>& vfeatures, vector<int>& vlabels, string model_file);
 void svm_test(vector<vector<double>>& vfeatures, vector<int>& vlabels, string model_file);
 string decide(int classNo);
-void extractFeature(string srcPath, vector<vector<double>>& features, vector<int>& labels, bool save_immediate_result, string mhiDir, string adiDir, string ddiDir);
+
 void saveFeature(vector<vector<double>>& features, vector<int>& labels, string featureFile);
 void loadFeature(vector<vector<double>>& features, vector<int>& labels, string featureFile);
 void split(string line, string separator, vector<double>& data);
+void combineData(vector<vector<double>>& feature, vector<vector<double>>& feature1, vector<vector<double>>& feature2, vector<vector<double>>& feature3);
+void dataToLibSVM(vector<vector<double>>& vfeatures, vector<int>& vlabels, string outputFile);
+
+void extractFeature(string srcPath, vector<vector<double>>& features, vector<int>& labels, bool save_immediate_result, string mhiDir, string adiDir, string ddiDir);
 
 void extractFeatureFromHBB(IplImage* ddiImg, vector<double>& feature, int start);
 void box(IplImage* img, int horPatches, int verPatches, vector<int>& win);
@@ -30,8 +33,8 @@ void HuMoment(IplImage* img, vector<double>& feature);
 long long getMpq(IplImage* img,int p, int q);
 long long getUpq(IplImage* img, double xbar, double ybar, int p, int q);
 
-void init(IplImage *img, int pixel);
-void binary(IplImage *img, int pixel);
+void initialize(IplImage *img, int value);
+void binaryProcessing(IplImage *img, int value);
 void getMHI(string srcPath, string srcFile, IplImage *mhiImg, bool isSave, string destFile);
 void getADI(string srcPath, string srcFile, IplImage *adiImg, bool isSave, string destFile);
 void getDDI(string srcPath, string srcFile, IplImage *ddiImg, bool isSave, string destFile);
@@ -105,10 +108,10 @@ int main( int argc, char** argv ){
 	//svm_train(train_features, train_labels, model_108);
 
 	//Testing 
-	loadFeature(test_features, test_labels, test_108);
+	//loadFeature(test_features, test_labels, test_108);
 	//svm_test(test_features, test_labels, model_108);
 	
-	dataToLibSVM(test_features, test_labels, "..\\Datasets\\Features\\Split\\test_108");
+	//dataToLibSVM(train_features, train_labels, "..\\Datasets\\Features\\Split\\train_108");
 /*******************************Using 14 features****************************************************/
 	
 	string train_14="..\\Datasets\\Features\\Split\\train_14.data";
@@ -131,27 +134,23 @@ int main( int argc, char** argv ){
 	//svm_test(test_features, test_labels, model_14);
 	
 	//dataToLibSVM(train_features, train_labels, "..\\Datasets\\Features\\Split\\train_14");
-/***************************************************************************************************/
+/***********************************Combine Features*************************************************/
+	vector<vector<double>> feature1, feature2, feature3, cfeature;
+	vector<int> label1, label2, label3;
+
+	//loadFeature(feature1, label1, "..\\Datasets\\Features\\Split\\MHI\\test_108.data");
+	//loadFeature(feature2, label2, "..\\Datasets\\Features\\Split\\DDI\\test_108.data");
+	//loadFeature(feature3, label3, "..\\Datasets\\Features\\Split\\ADI\\test_108.data");
+	//combineData(cfeature, feature1, feature2, feature3);
+	//saveFeature(cfeature, label1, "..\\Datasets\\Features\\Split\\All_Data_Test.txt");
+	
+	//loadFeature(feature1, label1, "..\\Datasets\\Features\\Split\\All_Data_Train.txt");
+	//dataToLibSVM(feature1, label1, "..\\Datasets\\Features\\Split\\All_Data_Train");
+
 	train_features.clear(), test_features.clear(), train_labels.clear(), test_labels.clear();
+	feature1.clear(), feature2.clear(), feature3.clear(), cfeature.clear();
+	label1.clear(), label2.clear(), label3.clear();
 	return 0;
-}
-
-void dataToLibSVM(vector<vector<double>>& vfeatures, vector<int>& vlabels, string outputFile){
-	ofstream out(outputFile);
-	if(!out){
-		cout<<"Unable to open output file"<<endl;
-		exit(0);
-	}
-
-	for(int i=0; i<vfeatures.size(); i++){
-		out<<vlabels[i]<<" ";
-		for(int j=0; j<vfeatures[0].size(); j++){
-			double v=vfeatures[i][j];
-			//if(v!=0.0)
-				out<<(j+1)<<":"<<vfeatures[i][j]<<" ";
-		}
-		out<<endl;
-	}
 }
 
 void svm_train(vector<vector<double>>& vfeatures, vector<int>& vlabels, string model_file){
@@ -258,9 +257,9 @@ void extractFeature(string srcPath, vector<vector<double>>& features, vector<int
 			getADI(dir, files[j], adiImg, save_immediate_result, adiFile.str());
 			getDDI(dir, files[j], ddiImg, save_immediate_result, ddiFile.str());
 
-			vector<double> feature=vector<double>(122, 0);
-			extractFeatureFromHu(mhiImg, adiImg, feature, 0);
-			extractFeatureFromHBB(ddiImg, feature, 14);
+			vector<double> feature=vector<double>(108, 0);
+			//extractFeatureFromHu(mhiImg, adiImg, feature, 0);
+			extractFeatureFromHBB(ddiImg, feature, 0);
 			features.push_back(feature);
 			labels.push_back(classNo);
 			feature.clear();
@@ -319,6 +318,38 @@ void split(string line, string separator, vector<double>& data){
 	}
 	str=line.substr(lastPosition, line.size()-lastPosition);
 	data.push_back(atof(str.c_str()));
+}
+
+void combineData(vector<vector<double>>& feature, vector<vector<double>>& feature1, vector<vector<double>>& feature2, vector<vector<double>>& feature3) {
+	for(int i=0; i<feature1.size(); i++){
+		vector<double> f;
+		for(int j=0; j<feature1[i].size(); j++)
+			f.push_back(feature1[i][j]);
+		for(int j=0; j<feature2[i].size(); j++)
+			f.push_back(feature2[i][j]);
+		for(int j=0; j<feature3[i].size(); j++)
+			f.push_back(feature3[i][j]);
+		feature.push_back(f);
+		f.clear();
+	}
+}
+
+void dataToLibSVM(vector<vector<double>>& vfeatures, vector<int>& vlabels, string outputFile){
+	ofstream out(outputFile);
+	if(!out){
+		cout<<"Unable to open output file"<<endl;
+		exit(0);
+	}
+
+	for(int i=0; i<vfeatures.size(); i++){
+		out<<vlabels[i]<<" ";
+		for(int j=0; j<vfeatures[0].size(); j++){
+			double v=vfeatures[i][j];
+			//if(v!=0.0)
+				out<<(j+1)<<":"<<vfeatures[i][j]<<" ";
+		}
+		out<<endl;
+	}
 }
 
 void extractFeatureFromHBB(IplImage* ddiImg, vector<double>& feature, int start){
@@ -410,90 +441,6 @@ void extractFeatureFromHu(IplImage* mhiImg, IplImage* adiImg, vector<double>& fe
 	feature1.clear(), feature2.clear();
 }
 
-void HuMoment1(IplImage* img, vector<double>& feature)
-{
-	int bmpWidth = img->width;
-	int bmpHeight = img->height;
-	int bmpStep = img->widthStep;
-	int bmpChannels = img->nChannels;
-	uchar*pBmpBuf = (uchar*)img->imageData;
-
-	double m00=0,m11=0,m20=0,m02=0,m30=0,m03=0,m12=0,m21=0;   //中心矩
-	double x0=0,y0=0;    //计算中心距时所使用的临时变量（x-x'）
-	double u20=0,u02=0,u11=0,u30=0,u03=0,u12=0,u21=0;  //规范化后的中心矩
-	
-	//double M[7]; //HU不变矩
-	double t1=0,t2=0,t3=0,t4=0,t5=0;   //临时变量
-	//double Center_x=0,Center_y=0;//重心
-	int Center_x=0,Center_y=0;        //重心
-	int i,j;     //循环变量
-
-	// 获得图像的区域重心
-	double s10=0,s01=0,s00=0;    //0阶矩和1阶矩 //注：二值图像的0阶矩表示面积
-	for(j=0;j<bmpHeight;j++)     //y
-	{
-		for(i=0;i<bmpWidth;i++)  //x
-		{
-			s10+=i*pBmpBuf[j*bmpStep+i];
-			s01+=j*pBmpBuf[j*bmpStep+i];
-			s00+=pBmpBuf[j*bmpStep+i];
-		}
-	}
-	Center_x=(int)(s10/s00+0.5);
-	Center_y=(int)(s01/s00+0.5);
-
-	// 计算二阶、三阶矩
-	m00=s00;
-	for(j=0;j<bmpHeight;j++)
-	{
-		for(i=0;i<bmpWidth;i++)   //x
-		{
-			x0=(i-Center_x);
-			y0=(j-Center_y);
-			m11+=x0*y0*pBmpBuf[j*bmpStep+i];
-			m20+=x0*x0*pBmpBuf[j*bmpStep+i];
-			m02+=y0*y0*pBmpBuf[j*bmpStep+i];
-			m03+=y0*y0*y0*pBmpBuf[j*bmpStep+i];
-			m30+=x0*x0*x0*pBmpBuf[j*bmpStep+i];
-			m12+=x0*y0*y0*pBmpBuf[j*bmpStep+i];
-			m21+=x0*x0*y0*pBmpBuf[j*bmpStep+i];
-		}
-	}
-	
-	// 计算规范化后的中心矩
-	u20=m20/pow(m00,2);
-	u02=m02/pow(m00,2);
-	u11=m11/pow(m00,2);
-	u30=m30/pow(m00,2.5);
-	u03=m03/pow(m00,2.5);
-	u12=m12/pow(m00,2.5);
-	u21=m21/pow(m00,2.5);
-
-	// 计算中间变量
-	t1=(u20-u02);
-	t2=(u30-3*u12);
-	t3=(3*u21-u03);
-	t4=(u30+u12);
-	t5=(u21+u03);
-    double M[7] = {0}; 
-	// 计算不变矩
-	M[0]=u20+u02;
-	M[1]=t1*t1+4*u11*u11;
-	M[2]=t2*t2+t3*t3;
-	M[3]=t4*t4+t5*t5;
-	M[4]=t2*t4*(t4*t4-3*t5*t5)+t3*t5*(3*t4*t4-t5*t5);
-	M[5]=t1*(t4*t4-t5*t5)+4*u11*t4*t5;
-	M[6]=t3*t4*(t4*t4-3*t5*t5)-t2*t5*(3*t4*t4-t5*t5);
-
-    feature[0]=M[0];
-	feature[1]=M[1];
-	feature[2]=M[2];
-	feature[3]=M[3];
-	feature[4]=M[4];
-	feature[5]=M[5];
-	feature[6]=M[6];
-}
-
 void HuMoment(IplImage* img, vector<double>& feature){
 	long long M00=getMpq(img, 0, 0);
 	long long M01=getMpq(img, 0, 1);
@@ -559,11 +506,12 @@ void getMHI(string srcPath, string srcFile, IplImage *mhiImg, bool isSave, strin
 
 	CvSize size=cvSize(width, height);
 	IplImage *tmhi=cvCreateImage(size, IPL_DEPTH_8U, channels);
-	uchar *tmhiData=(uchar*)tmhi->imageData;
-	uchar *mhiData=(uchar*)mhiImg->imageData;
 
-	init(mhiImg, 0);
-	init(tmhi, 32);
+	initialize(mhiImg, 0);
+	initialize(tmhi, 32);
+
+	uchar *tmhiData=(uchar*)tmhi->imageData;
+	uchar *mhiData=(uchar*)mhiImg->imageData;	
 
 	cout<<"MHI: "<<srcFile<<endl;
 	string file=srcPath.append("\\").append(srcFile);
@@ -582,8 +530,8 @@ void getMHI(string srcPath, string srcFile, IplImage *mhiImg, bool isSave, strin
 	curFrame=cvQueryFrame(curCapture);
 	curFrame=cvQueryFrame(curCapture);
 	do{
-		binary(preFrame, 1);
-		binary(curFrame, 1);
+		binaryProcessing(preFrame, 1);
+		binaryProcessing(curFrame, 1);
 		preData=(uchar*)preFrame->imageData;
 		curData=(uchar*)curFrame->imageData;
 		for(int w=0; w<width; w++){
@@ -618,31 +566,31 @@ void getMHI(string srcPath, string srcFile, IplImage *mhiImg, bool isSave, strin
 		cvSaveImage(destFile.c_str(), mhiImg);
 }
 
-void init(IplImage *img, int pixel){
+void initialize(IplImage *img, int value){
 	int width=img->width, height=img->height, channels=img->nChannels, step=img->widthStep;
 	uchar* data=(uchar*)img->imageData  ;
 
 	for(int w=0; w<width; w++){
 		for(int h=0; h<height; h++){
-			data[h*step+w*channels+0]=pixel ;
-			data[h*step+w*channels+1]=pixel ;
-			data[h*step+w*channels+2]=pixel  ;
+			data[h*step+w*channels+0]=value ;
+			data[h*step+w*channels+1]=value ;
+			data[h*step+w*channels+2]=value  ;
 		}
 	}
 }
 
-void binary(IplImage *img, int pixel){
+void binaryProcessing(IplImage *img, int value){
 	int width=img->width, height=img->height, channels=img->nChannels, step=img->widthStep;
 
 	uchar *data=(uchar*)img->imageData;
 	for(int w=0; w<width; w++){
 		for(int h=0; h<height; h++){
 			if(data[h*step+w*channels] != 0)
-				data[h*step+w*channels+0]=pixel;
+				data[h*step+w*channels+0]=value;
 			if(data[h*step+w*channels+1] != 0)
-				data[h*step+w*channels+1]=pixel;
+				data[h*step+w*channels+1]=value;
 			if(data[h*step+w*channels+2] != 0)
-				data[h*step+w*channels+2]=pixel;
+				data[h*step+w*channels+2]=value;
 		}
 	}
 }
